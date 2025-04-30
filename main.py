1c5903c568fae1c648bfd7cfb75dcb6412d34f96
pilipili-hash <celestinabashiriancb@gmail.com>
2025-04-30 20:45:30 +0800

    1

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from utils.config_manager import load_config
 
 # _log = get_log()
 
-config.set_bot_uin("1554688500")  # 设置 bot qq 号 (必填)
+config.set_bot_uin("")  # 设置 bot qq 号 (必填)
 config.set_ws_uri("ws://localhost:3001")  # 设置 napcat websocket server 地址
 # config.set_token("napcat") # 设置 token (napcat 服务器的 token)
-config.set_root("1075047189")
+config.set_root("")
 bot = BotClient()
 load_config()  # 加载全局配置
 
